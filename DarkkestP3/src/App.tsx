fe08c3b6362f3959f26f073c79a141a1377572c3
Dean <omegadeangrey@gmail.com>
2024-05-23 13:14:07 -0400

    fixed your opp page

--- DarkkestP3/src/App.tsx
+++ DarkkestP3/src/App.tsx
@@ -4,23 +4,26 @@ import { Routes, Route } from "react-router-dom";
 // pages
 import LandingPage from "./pages/LandingPage";
 import AuthPage from "./pages/AuthPage"; // New AuthPage component
-import HomePage from "./pages/HomePage";
-import ViewOpportunitiesPage from "./pages/ViewOpportunitiesPage.tsx";
-import ProfilePage from "./pages/ProfilePage.tsx";
-import CreateOpportunitiesPage from "./pages/CreateOpportunityPage.tsx";
+// import HomePage from "./pages/HomePage";
+import OpportunityPage from "./pages/OpportunityPage.tsx";
+// import ProfilePage from "./pages/ProfilePage.tsx";
+// import CreateOpportunitiesPage from "./pages/CreateOpportunityPage.tsx";
 import NavBar from "./components/NavBar.tsx";
-import ApplicationPage from "./pages/ApplicationPage.tsx";
+// import ApplicationPage from "./pages/ApplicationPage.tsx";
 import { useEffect, useState } from "react";
 import { SelectedAuthForm, SelectedPage } from "./shared/types.ts";
 import Hero from "./pages/Hero.tsx";
 import Profile from "./pages/Profile.tsx";
 
-
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
-  const [selectedAuthForm, setSelectedAuthForm] = useState<SelectedAuthForm>(SelectedAuthForm.Register);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [selectedAuthForm, setSelectedAuthForm] = useState<SelectedAuthForm>(
+    SelectedAuthForm.Register
+  );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
-  
+
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY === 0) {
@@ -36,17 +39,30 @@ function App() {
   return (
     <div className="app">
       <AuthProvider>
-        <NavBar 
+        <NavBar
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage}
           setSelectedAuthForm={setSelectedAuthForm}
           isTopOfPage={isTopOfPage}
         />
         <Routes>
-          <Route path ="/" element = {<Hero setSelectedAuthForm={setSelectedAuthForm}/>} />
-          <Route path="/auth" element={<AuthPage selectedAuthForm={selectedAuthForm} setSelectedAuthForm={setSelectedAuthForm} />} />
+          <Route
+            path="/"
+            element={<Hero setSelectedAuthForm={setSelectedAuthForm} />}
+          />
+          <Route
+            path="/auth"
+            element={
+              <AuthPage
+                selectedAuthForm={selectedAuthForm}
+                setSelectedAuthForm={setSelectedAuthForm}
+              />
+            }
+          />
           <Route path="/landing" element={<LandingPage />} />
           <Route path="/profile" element={<Profile />} />
+
+          <Route path="/your-opportunities" element={<OpportunityPage />} />
           {/*
           <Route path="/opp" element={<ViewOpportunitiesPage />} />
           <Route path="/pro" element={<ProfilePage />} />
@@ -56,7 +72,6 @@ function App() {
         </Routes>
       </AuthProvider>
     </div>
-
   );
 }
 
---
+++ DarkkestP3/src/pages/OpportunityPage.tsx
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from "react";
+import { GetUserOpps } from "../services/opportunityService";
+import LeftSideBar from "../components/LeftSideBar";
+import OpportunitiesList from "../components/OpportunitiesList";
+import OpportunityFormModal from "../components/OpportunityFormModal";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { CreateOpp } from "../services/opportunityService";
+
+function OpportunityPage() {
+  interface Opportunity {
+    oppId: number;
+    appUserId: string;
+    jobTitle: string;
+    description: string;
+  }
+
+  const [oppId, setoppIdState] = useState(0);
+  const [appUserId, setappUserIdState] = useState("");
+  const [JobTitle, setJobTitleState] = useState("");
+  const [Description, setDescriptionState] = useState("");
+  const [userOpportunities, setUserOpportunities] = useState<Opportunity[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchUserOpportunities = async () => {
+      try {
+        const res = await GetUserOpps();
+        const data = await res;
+        setUserOpportunities(data);
+      } catch (error) {
+        console.error("Failed to fetch opportunities:", error);
+      }
+    };
+    fetchUserOpportunities();
+  }, []);
+
+  // function setIsModalOpen(arg0: boolean): void {
+  //   throw new Error("Function not implemented.");
+  // }
+
+  const filteredOpportunities = userOpportunities.filter((userOpportunity) =>
+    userOpportunity.jobTitle.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const opportunityData = {
+      JobTitle,
+      Description,
+    };
+    try {
+      const res = await CreateOpp(opportunityData);
+      if (!res || !res.ok) {
+        throw new Error("Failed to create opportunity");
+      }
+      const newOpportunity = await res.json();
+      setUserOpportunities([
+        ...userOpportunities,
+        {
+          oppId: oppId,
+          appUserId: appUserId,
+          jobTitle: JobTitle,
+          description: Description,
+        },
+      ]);
+      setoppIdState(0);
+      setappUserIdState("");
+      setJobTitleState("");
+      setDescriptionState("");
+      setIsModalOpen(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <section className="min-h-screen flex flex-col bg-secondary-white pt-10 md:h-full">
+      <div className="flex h-full">
+        <div>
+          <LeftSideBar />
+        </div>
+        <div className="flex-2 bg-white w-2/4 pt-20 p-4">
+          <div className="flex justify-between items-center mb-4">
+            <h1
+              className="text-2xl font-semibold leading-tight"
+              style={{ fontFamily: "Lato, sans-serif", fontWeight: 400 }}
+            >
+              Opportunities
+            </h1>
+            <button
+              className="bg-white text-black font-semibold py-2 px-4 rounded border border-primary-green hover:bg-primary-green hover:text-white transition duration-200"
+              onClick={() => setIsModalOpen(true)}
+            >
+              Create New Opportunity
+            </button>
+          </div>
+          <div className="relative">
+            {/* Search Bar */}
+            <input
+              type="text"
+              placeholder="Search for opportunities"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="border rounded p-2 w-full pl-10"
+              style={{ fontFamily: "Lato, sans-serif", fontWeight: 400 }}
+            />
+            <MagnifyingGlassIcon className="h-5 w-5 absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500" />
+          </div>
+          <div className="mt-8 text-left px-8">
+            <div className="mt-8 flex flex-col space-x-4 justify-center">
+              {/* Display OpportunitiesList */}
+              <OpportunitiesList opportunities={filteredOpportunities} />
+            </div>
+          </div>
+        </div>
+        {/* Modal for creating new opportunities */}
+        <OpportunityFormModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          onSubmit={handleSubmit}
+          JobTitle={JobTitle}
+          Description={Description}
+          setJobTitleState={setJobTitleState}
+          setDescriptionState={setDescriptionState}
+        />
+      </div>
+    </section>
+  );
+}
+export default OpportunityPage;
